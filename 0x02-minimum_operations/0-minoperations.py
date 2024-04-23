b65e891a099d770fe0579e675c70faaa0c52f751
oussamaNAJIH <oussamanajih6@gmail.com>
2024-04-23 19:56:38 +0200

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+minOperations(n) module
+"""
+
+
+def minOperations(n):
+    """
+     calculates the fewest number of operations needed to
+     result in exactly n H characters in the file
+    """
+    if n <= 0:
+        return 0
+    result = "H"
+    tmp = ""
+    num_operations = 0
+    while len(result) < n:
+        if n % len(result) == 0:
+            tmp = result
+            result += tmp
+            num_operations += 2
+            continue
+        elif n % len(result) != 0:
+            result += tmp
+            num_operations += 1
+            continue
+    return num_operations
