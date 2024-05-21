738eb189a3b9958c2c7dc90dcc2ae7d346a9b74b
oussamaNAJIH <oussamanajih6@gmail.com>
2024-05-21 22:25:11 +0200

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,38 +1,29 @@
 #!/usr/bin/node
 const request = require('request');
 
-function getData (url) {
+async function getData (url) {
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else if (response.statusCode !== 200) {
-        reject(new Error(`Request failed with status code: ${response.statusCode}`));
-      } else {
+      if (!error && response.statusCode === 200) {
         resolve(JSON.parse(body));
+      } else {
+        reject(Error('Error encountered fetching data'));
       }
     });
   });
 }
 
-async function main () {
-  const movieId = parseInt(process.argv[2]);
+function main () {
+  const characterURLS = [];
+  const movieId = process.argv[2];
   const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-  try {
-    const data = await getData(movieUrl);
-    const characters = data.characters;
-    const characterPromises = characters.map(characterUrl => getData(characterUrl));
-
-    const characterData = await Promise.all(characterPromises);
-    characterData.forEach(character => {
-      if (character) {
-        console.log(character.name);
-      }
-    });
-  } catch (error) {
-    console.error(error);
+  const movieData = getData(movieUrl);
+  for (const characterURL of movieData.characters) {
+    characterURLS.push(getData(characterURL));
+  }
+  const characters = Promise.all(characterURLS);
+  for (const character of characters) {
+    console.log(character.name);
   }
 }
-
 main();
