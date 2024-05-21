afbc9fe867c462e501ef901974a59e653c06e78f
oussamaNAJIH <oussamanajih6@gmail.com>
2024-05-21 23:11:04 +0200

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
-async function getData (url) {
+function getData (url) {
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
       if (!error && response.statusCode === 200) {
@@ -13,15 +13,15 @@ async function getData (url) {
   });
 }
 
-async function main () {
+function main () {
   const characterURLS = [];
   const movieId = process.argv[2];
   const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-  const movieData = await getData(movieUrl).then(result => { return result; });
+  const movieData = getData(movieUrl).then(result => { return result; });
   for (const characterURL of movieData.characters) {
     characterURLS.push(getData(characterURL));
   }
-  const characters = await Promise.all(characterURLS).then((values) => {
+  const characters = Promise.all(characterURLS).then((values) => {
     return values;
   });
   for (const character of characters) {
