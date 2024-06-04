c84f868dd2e64d4f1b995543fc728c67d33d2d51
oussamaNAJIH <oussamanajih6@gmail.com>
2024-06-05 01:45:48 +0200

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,21 +4,21 @@ modeule for makeChange
 """
 
 
-def makeChange(coins, total):
-    """
-    Return: fewest number of coins needed to meet total
-    """
-    if total <= 0:
-        return 0
+# def makeChange(coins, total):
+#     """
+#     Return: fewest number of coins needed to meet total
+#     """
+#     if total <= 0:
+#         return 0
 
-    # Initialize dp array with infinity values, except dp[0] which is 0
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+#     # Initialize dp array with infinity values, except dp[0] which is 0
+#     dp = [float('inf')] * (total + 1)
+#     dp[0] = 0
 
-    # Build up the dp array
-    for coin in coins:
-        for x in range(coin, total + 1):
-            if dp[x - coin] != float('inf'):
-                dp[x] = min(dp[x], dp[x - coin] + 1)
+#     # Build up the dp array
+#     for coin in coins:
+#         for x in range(coin, total + 1):
+#             if dp[x - coin] != float('inf'):
+#                 dp[x] = min(dp[x], dp[x - coin] + 1)
 
-    return dp[total] if dp[total] != float('inf') else -1
+#     return dp[total] if dp[total] != float('inf') else -1
