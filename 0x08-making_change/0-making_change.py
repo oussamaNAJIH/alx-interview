6237115d51e9e5ef76d5e884791d960f2590e2e5
oussamaNAJIH <oussamanajih6@gmail.com>
2024-06-06 17:19:54 +0200

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
+
 """
-modeule for makeChange
+Module to solve the coin change problem using dynamic
+programming with optimized space complexity
 """
 
 
-# def makeChange(coins, total):
-#     """
-#     Return: fewest number of coins needed to meet total
-#     """
-#     if total <= 0:
-#         return 0
+def makeChange(coins, total):
+    """
+    Determine the fewest number of coins needed to meet a given amount total.
 
-#     # Initialize dp array with infinity values, except dp[0] which is 0
-#     dp = [float('inf')] * (total + 1)
-#     dp[0] = 0
+    Args:
+        coins (list): List of the values of the coins in your possession.
+        total (int): The total amount to be met with the coins.
 
-#     # Build up the dp array
-#     for coin in coins:
-#         for x in range(coin, total + 1):
-#             if dp[x - coin] != float('inf'):
-#                 dp[x] = min(dp[x], dp[x - coin] + 1)
+    Returns:
+        int: Fewest number of coins needed to meet the total,
+             or -1 if the total cannot be met by any number of coins.
+    """
+    if total <= 0:
+        return 0
 
-#     return dp[total] if dp[total] != float('inf') else -1
+    # Initialize dp array with infinity values, except dp[0] which is 0
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    # Build up the dp array
+    for coin in coins:
+        for x in range(coin, total + 1):
+            dp[x] = min(dp[x], dp[x - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
